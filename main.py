784411fa3b7d23b884019b343c649b7f64eaed2f
reto <info@rutishauser-ag.ch>
2021-05-14 17:44:23 +0200

    add camera

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from datetime import datetime, time
 from kivy.app import App
 from kivy.factory import Factory
 from kivy.properties import StringProperty
+from kivy.uix.camera import Camera
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
