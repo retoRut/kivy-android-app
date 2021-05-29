eb54f6fd0330dc24e9fdf038f70580c7516faf34
reto <info@rutishauser-ag.ch>
2021-05-29 11:32:10 +0200

    redesign Application

--- connector.py
+++ connector.py
@@ -1,6 +1,7 @@
 import requests
 import json
 
+from main import Scroll
 
 
 class ClientService:
@@ -9,31 +10,33 @@ class ClientService:
     # password = 'api-rutag-api'
     #password = 'retorutishauser1976'
 
-    def __init__(self, backend, apikey):
+    def __init__(self, backend, apikey, main_app, main_layout):
         """
             init
         :param backend: url of backend
         :param apikey:  access key
         """
         self.backend = backend
+        self.main_app = main_app
         #self.key = apikey
         self.headers = {
             'Authorization': 'Bearer ' + str(apikey),
             'Content-Type': 'application/json',
         }
+        self.communication_screen = Scroll( main_app)
 
 
 
 
 class JoomlaConnectorService(ClientService):
 
-    def __init__(self, backend, apikey):
+    def __init__(self, backend, apikey, main_app, main_layout):
         """
 
         :param backend: url of backend
         :param apikey: access key
         """
-        super().__init__(backend, apikey)
+        super().__init__(backend, apikey,main_app, main_layout)
 
     def get_all_articles(self):
         """
@@ -46,18 +49,34 @@ class JoomlaConnectorService(ClientService):
 
     def get_article(self, alias):
         """
-
-        :param alias:
+            get an article based on alias Name
+        :param alias: alias of the article
         :return:
         """
         url = self.backend+'/api/index.php/v1/content/article'
         response = requests.request('GET', url, headers=self.headers)
         #jprint(response.json())
+        self.communication_screen.add_text_to_screen('get_article: received status Code:' + str(response.status_code), 'communication')
         res = response.json()
+        # print(res)
         for i in res['data']:
             if i['attributes']['alias'] == alias:
                 return i['attributes']
         return False
+
+    def delete_article(self, id):
+        """
+         Delete an Article
+        :param id: id of article to be deleted
+        :return:
+        """
+        url = self.backend+'/api/index.php/v1/content/article/'+str(id)
+        # response = requests.request('PATCH', url, headers=self.headers)
+        response = requests.request("DELETE", url, headers=self.headers)
+#        print(response.json())
+        self.communication_screen.add_text_to_screen('delete_article: received status Code:' + str(response.status_code), 'communication')
+     #   res = response.json()
+        return True
 
     def get_all_categories(self):
         """
@@ -83,14 +102,13 @@ class JoomlaConnectorService(ClientService):
                 return str(i['attributes']['id'])
         return False
 
-    def create_new_article(self, alias, title, text, catID, mainLayout):
+    def create_new_article(self, alias, title, text, catID):
         """
-
+         Create a new Article
         :param alias:
         :param title:
         :param text:
         :param catID:
-        :param mainLayout:
         :return:
         """
         url = self.backend+'/api/index.php/v1/content/article'
@@ -105,7 +123,7 @@ class JoomlaConnectorService(ClientService):
         })
         #self.jprint.jprint(post)
         response = requests.request('POST', url, headers=self.headers, data=post)
-        mainLayout.root.ids.communicationScreen_Label.text = mainLayout.root.ids.communicationScreen_Label.text+ '\n' + 'received status Code:'+str(response.status_code)
+        self.communication_screen.add_text_to_screen('create_new_article: received status Code:' + str(response.status_code), 'communication')
         print(response.headers)
         print(response.text.encode('utf8'))
 
@@ -131,7 +149,7 @@ class JoomlaConnectorService(ClientService):
         response = requests.request('GET', url, headers=self.headers)
         return response.json()
 
-    def get_all_contacts_by_name_and_building(self, building):
+    def get_all_contacts_by_name_and_building(self, catid):
         """
             get all names from the contacts (not homepage user) of the joomla homepage
         :return: contact names as list
@@ -141,10 +159,9 @@ class JoomlaConnectorService(ClientService):
         url = self.backend+'/api/index.php/v1/contact'
         response = requests.request('GET', url, headers=self.headers)
         res = response.json()
-        text = json.dumps(res, sort_keys=True, indent=4)
-        print(text)
+        print(json.dumps(res,  indent=4, sort_keys=True))
         for user in res['data'] :
-            if building == user['attributes']['catid']:
+            if catid == user['attributes']['catid']:
                 userlist.append(user['attributes']['name'])
             #self.jprint.jprint(user['attributes'])
         return userlist
--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import json
 from datetime import datetime, time
+from html.parser import HTMLParser
 
 from kivy.app import App
+from kivy.core.text import Label
 from kivy.factory import Factory
 from kivy.properties import StringProperty
+from kivy.uix.popup import Popup
 from kivy.uix.tabbedpanel import TabbedPanel
+# library for html parsing:
+import lxml
+import re
+import pandas as pd
+from lxml import etree
+
 import connector
 
 # Enable logging
@@ -22,12 +31,11 @@ class MainApp(App):
         """
         print('build methode run at Main APP')
         main_layout = MainLayout(self)
+
         backend = 'https://rutishauser-ag.ch'
         apikey = 'c2hhMjU2OjM1OjI1N2RmNjRmY2UxYTFiMDE2ODcxMmY0YWE5NGYyYWQ4MzkwM2JkZjA2YTNkZmM3NzAwNGExZmE3MmUyYThmZTY='
-        self.clientService = connector.JoomlaConnectorService(backend, apikey)
+        self.clientService = connector.JoomlaConnectorService(backend, apikey, self, main_layout)
         self.logger = Logger()
-
-
        # main_layout.setMainApp(self)
         return main_layout
 
@@ -40,7 +48,9 @@ class MainLayout(TabbedPanel):
         print('init run at MainLayout ')
         self.MainApp = mainApp
         self.mieter = 0
-        self.building = 0
+        self.catid = 0
+        # init gui scroll screens
+        self.data_screen = Scroll(self.MainApp)
 
 
     def capture(self):
@@ -53,18 +63,83 @@ class MainLayout(TabbedPanel):
         camera.export_to_png("IMG_{}.png".format(timestr))
         print("Captured")
 
-
-
-    def periode_Spinner_values(self):
+    @staticmethod
+    def kassabuch_year_spinner_default_value():
         """
-            set spinner values of Nebenkosten period
-        :return: list of dates
+            set the default value at kassabuch on actual year
+        :return:
         """
-        list= ['1-7-2020-30-6-2021','1-7-2021-30-6-2022','1-7-2022-30-6-2023','1-7-2023-30-6-2024']
+        return datetime.now().strftime('%Y')
+
+    @staticmethod
+    def kassabuch_year_spinner_values():
+        """
+         set spinner values of kassabuch for three years
+        :return:list of years
+        """
+        list= [str(datetime.now().year),str(datetime.now().year+1),str(datetime.now().year+2)]
         # print(list)
         return list
 
-    def building_Spinner_values(self):
+    @staticmethod
+    def kassabuch_ea_spinner_default_value():
+        """
+
+        :return:
+        """
+        return 'Einnahmen'
+
+    @staticmethod
+    def kassabuch_ea_spinner_values():
+        """
+
+        :return:
+        """
+        list= ['Einnahmen','Ausgaben']
+        return list
+
+    def kassabuch_ea_select(self, text):
+        pass
+
+    @staticmethod
+    def kassabuch_fuer_spinner_values():
+        list= ['Waschgeld','Ausgaben']
+        return list
+
+    @staticmethod
+    def kassabuch_fuer_spinner_select(text):
+        pass
+
+    def kassabuch_prev_kassabuch_button_on_click(self):
+        """
+                get the actual kassabuch
+        :return:
+        """
+        # connector
+
+       # self.data_screen.set_label(self.MainApp.root.ids.dataScreen_Label)
+        #TODO: Auslesen Jahr kassabuch
+        resp = self.MainApp.clientService.get_article('kassabuch-2021' )
+
+   # self.communication_screen.add_text_to_screen(i['attributes'], 'data')
+        print(json.dumps(resp, indent=4, sort_keys=True))
+        # print(json.dumps(resp['text'], indent=4, sort_keys=True))
+        self.data_screen.add_text_to_screen(str(resp),'data')
+
+        self.data = DataStore(resp['text'])
+        text = self.data.get_html_text()
+        # get cat ID missing on article..
+        #TODO: catID Name should be not constant
+        catid = self.MainApp.clientService.get_categorie_id("Waschen")
+        self.MainApp.clientService.delete_article(resp['id'])
+        self.MainApp.clientService.create_new_article( 'kassabuch-2021', resp['title'], text, catid )
+        # self.MainApp.clientService.update_article(resp['id'],catid,resp['title'], text)
+        #print(table[1])
+        #e = df[df.Monat == 'januar']
+        #print(e)
+        # self.data_screen.add_text_to_screen(str(dfs), 'data')
+
+    def kassabuch_category_Spinner_values(self):
         """
 
         :return:
@@ -73,35 +148,33 @@ class MainLayout(TabbedPanel):
         # print(list)
         return list
 
-    def get_mieter_click(self):
+    def kassabuch_get_contacts_on_click(self):
         """
 
         :return:
         """
-        if(self.building ==0 ):
-            PopupApp().build("Please select a building first")
+        if(self.catid ==0 ):
+            PopupApp().build("Please select a category first")
         else:
-            list = self.MainApp.clientService.get_all_contacts_by_name_and_building(self.building)
+            list = self.MainApp.clientService.get_all_contacts_by_name_and_building(self.catid)
             print(list)
             self.MainApp.logger.set_log(self.MainApp.root.ids.communicationScreen_Label, ' connect homepage' + '\n')
             self.MainApp.logger.set_log(self.MainApp.root.ids.communicationScreen_Label,
-                                     ' get all contacts of building '+str(self.building)+ '\n' )
-            self.ids.mieter_Spinner.values = list
-            self.MainApp.root.ids.mieter_Spinner.disabled = False
+                                     ' get all contacts of category '+str(self.catid)+ '\n' )
+            self.ids.contacts_Spinner.values = list
+            self.MainApp.root.ids.contacts_Spinner.disabled = False
 
-    def get_spinner_mieter_values(self):
-        '''
-         get all Clients (mieter from the homepage)
+    def kassabuch_contacts_spinner_values(self):
+        """
+            set default value of contacts
         :return:
-        '''
-        print('run get_spinner_mieter_values')
-
+        """
         # list = self.clientService.get_all_contacts_by_name()
         list= ['1','2']
         # print(list)
         return list
 
-    def on_periode_select(self, text):
+    def kassabuch_year_select(self, text):
         pass
 
     def on_init_click(self):
@@ -146,25 +219,28 @@ class MainLayout(TabbedPanel):
 
         #pops.open()
 
-    def on_preview_click(self):
+    @staticmethod
+    def kassabuch_preview_entry_button_on_click():
         """
         add an entry to list
         :return:
         """
+        # creating entry with the following:
+
         #create the new one:
-        tmp = self.MainApp.root.ids.periode_Spinner.text+','\
-                      +self.MainApp.root.ids.building_Spinner.text+',' \
-                      + self.MainApp.root.ids.mieter_Spinner.text + ',' \
-                       +self.MainApp.root.ids.date.text+', '\
-                        +self.MainApp.root.ids.cost_TextInput.text+';\n'
+        tmp = MainApp.root.ids.periode_Spinner.text+','\
+                      +MainApp.root.ids.building_Spinner.text+',' \
+                      + MainApp.root.ids.mieter_Spinner.text + ',' \
+                       +MainApp.root.ids.date.text+', '\
+                        +MainApp.root.ids.cost_TextInput.text+';\n'
         # read the exsisting Text
-        self.MainApp.root.ids.dataScreen_Label.text = self.MainApp.root.ids.dataScreen_Label.text + tmp
+        MainApp.root.ids.dataScreen_Label.text = self.MainApp.root.ids.dataScreen_Label.text + tmp
         #print(image_to_string(Image.open('20180414_104309.jpg')))
  #       pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tessdata'
      #   pytesseract.pytesseract.tesseract_cmd = "C:\\Temp\\kivy-android-app\\venv\\Lib\\site-packages\\tesseract"
       #  print(pytesseract.image_to_string(Image.open(r'C:\Temp\kivy-android-app\20180414_104309.jpg')))
  #       print(pytesseract.image_to_string(Image.open('20180414_104309.jpg')))
-        self.MainApp.root.ids.cl.disabled = False
+        MainApp.root.ids.cl.disabled = False
 
     def on_clear_click(self):
         '''
@@ -174,8 +250,11 @@ class MainLayout(TabbedPanel):
         self.MainApp.root.ids.dataScreen_Label.text =''
         # Calendar()
 
-    def get_haus_click(self):
+    def kassabuch_get_categories_on_click(self):
+        """
 
+        :return:
+        """
         self.contact_categories = self.MainApp.clientService.get_all_contact_categories()
         tmp_cat = []
         for cat in self.contact_categories :
@@ -185,10 +264,12 @@ class MainLayout(TabbedPanel):
         self.MainApp.logger.set_log(self.MainApp.root.ids.communicationScreen_Label, ' connect homepage' + '\n')
         self.MainApp.logger.set_log(self.MainApp.root.ids.communicationScreen_Label,
                                      ' get all categories of building '+ '\n' )
-        self.ids.building_Spinner.values = tmp_cat
-        self.MainApp.root.ids.building_Spinner.disabled = False
 
-    def on_mieter_select(self, text):
+        self.MainApp.root.ids.category_Spinner.values = tmp_cat
+        self.MainApp.root.ids.category_Spinner.disabled = False
+        self.MainApp.root.ids.get_contacts.disabled = False
+
+    def kassabuch_contacts_spinner_select(self, text):
         """
 
         :param text:
@@ -206,19 +287,16 @@ class MainLayout(TabbedPanel):
        # self.MainApp.root.ids.cl.disabled = False
         self.MainApp.root.ids.send.disabled = False
 
-
-    def on_building_select(self,cat_title):
+    def kassabuch_category_spinner_select(self, text):
         """
 
-        :param id:
+        :param text:
         :return:
         """
-        print("cat Title: "+cat_title)
-        for cat in self.contact_categories:
-            print(cat)
-            if cat['attributes']['title'] == cat_title:
-                self.building = cat['attributes']['id']
-                break
+        for cat in self.contact_categories :
+            if not cat['attributes']['title'] == text:
+                self.catid = cat['attributes']['id']
+                return True
 
     def on_date_text(self):
 
@@ -238,9 +316,11 @@ class MainLayout(TabbedPanel):
 
 class PopupApp():
 
-    def build(self, text):
+    def build(self, text, title = None):
         popup = Factory.ErrorPopup()
         popup.message.text = text
+        if title:
+            popup.title = title
         popup.open()
 
 class Test(TabbedPanel):
@@ -261,6 +341,110 @@ class Logger:
         :rtype: object
         """
         return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+
+class Scroll:
+    def __init__(self, main_app):
+        """
+          constructor
+        :param main_app:  reference to root
+        :param screen_label: label to be modified
+        """
+        self.main_app = main_app
+
+    def add_text_to_screen(self, t, screen):
+        """
+          add a text to the screen
+        :param text: text to be added
+        :return:
+        """
+
+        if screen == 'communication':
+            self.main_app.root.ids.communicationScreen_Label.text = self.main_app.root.ids.communicationScreen_Label.text + '\n' + str(
+                    t)
+        elif screen == 'data':
+            te = json.dumps(t, sort_keys=True, indent=4)
+            self.main_app.root.ids.dataScreen_Label.text = self.main_app.root.ids.dataScreen_Label.text + '\n' + str(
+                    te)
+
+
+class DataStore:
+    def __init__(self, html_text):
+        """
+
+        """
+        self.header_p = re.findall(r'<p>.*?</p>',html_text, re.DOTALL)
+        self.body_t = re.findall(r'<table.*?</table>',html_text, re.DOTALL)
+        # print("+++++++++++++++" + str(self.header_p)
+        # print("+++++++++++++++" + str(self.body_t))
+        #TODO: falls mehrere Tabelle übergeben wer
+        self.html_table = pd.read_html(self.body_t[0], header=0)[0]
+        print(self.html_table)
+        #df = dfs[0]
+
+    def modify_html_table(self, x_coord, y_coord, value):
+        """
+            modify an element of a table
+        :param x_coord: coordination x
+        :param y_coord: coordination y
+        :param value: new value
+        :return:
+        """
+        #print(dfs.loc[2,'Monat'])
+        # dfs.loc[2,'Monat'] = 'Februar'
+        self.dfs.loc[y_coord, x_coord] = value
+        print(self.dfs)
+
+    def add_entry(self,entry):
+        """
+
+        :param entry: list of string
+        :return:
+        """
+        print(self.dfs.index)
+        # hinzufuegen eines Elementes
+        # self.dfs.loc[len(self.dfs.index)] = ['März', '12.', 'reto', 'Waschgeld', '20', '20', '0']
+        #TODO: try catch? implementieren
+        if entry is list:
+            self.dfs.loc[len(self.dfs.index)] = entry
+            print(self.dfs)
+        else:
+            return None
+
+    def get_html_text(self):
+        """
+
+        :return:
+        """
+        table = self.html_table.to_html(index=False)
+        header = ''
+        for h in self.header_p:
+            header = header + h
+        text = header + table
+        print(text)
+        return text
+
+    def __repr__(self):
+        """
+            get all Data as string NOT html formated
+        :rtype: object
+        """
+        return "\n {0}  ".format(self.dataEntries)
+
+
+    def getAllEntriesByArg(self, arg ):
+        """
+
+        :param arg:
+        :return:
+        """
+        list = []
+        for entry in self.dataEntries:
+            if(arg =='name'):
+                list.append(entry.jsonObject['attributes']['name'])
+                print(entry.jsonObject['attributes']['name'])
+        return list
+
 
 if __name__ == "__main__":
     app = MainApp()
